bd4be789a2e8352ce08b1e23a7b146ffc7efcd76
feliposz <feliposz@gmail.com>
2017-09-09 02:54:16 -0300

    005 - Fixed buffer size, scaled drawing

--- code/main_handmade.js
+++ code/main_handmade.js
@@ -1,31 +1,44 @@
 "use strict";
 
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+var screen = document.createElement("canvas");
+var buffer = document.createElement("canvas");
 
 var bitmap;
 var BPP = 4; // Bytes per pixel
 
 function renderGreenBlueGradient(greenOffset, blueOffset) {
-    var x, y, r, g, b, a;
+    var x, y, r, g, b, a, rowOffset, columnOffset;
     for (y = 0; y < bitmap.height; y++) {
+        rowOffset = y*bitmap.width*BPP;
         for (x = 0; x < bitmap.width; x++) {
-            r = Math.floor(y * 255 / bitmap.height);
+            columnOffset = rowOffset + x*BPP;
+            r = Math.floor(y*255/bitmap.height);
             g = (x + greenOffset) % 256;
             b = (y + blueOffset) % 256;
             a = 255;
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 0] = r;
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 1] = g;
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 2] = b;
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 3] = a;
+            bitmap.data[columnOffset + 0] = r;
+            bitmap.data[columnOffset + 1] = g;
+            bitmap.data[columnOffset + 2] = b;
+            bitmap.data[columnOffset + 3] = a;
         }
     }
 }
 
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    bitmap = ctx.getImageData(0, 0, canvas.width, canvas.height);
+function displayBuffer() {
+    buffer.getContext("2d").putImageData(bitmap, 0, 0, 0, 0, buffer.width, buffer.height);
+    screen.getContext("2d").drawImage(buffer, 0, 0);
+}
+
+function resizeBuffer(width, height) {
+    buffer.width = width;
+    buffer.height = height;
+    bitmap = buffer.getContext("2d").getImageData(0, 0, buffer.width, buffer.height);
+}
+
+function resizeWindow() {
+    screen.width = window.innerWidth;
+    screen.height = window.innerHeight;    
+    screen.getContext("2d").scale(screen.width/buffer.width, screen.height/buffer.height);
     console.log("resize");
 }
 
@@ -36,18 +49,21 @@ function focus() {
 function main() {
     var greenOffset = 0, blueOffset = 0;
     var loop = function () {
-        renderGreenBlueGradient(greenOffset++, blueOffset++);
-        ctx.putImageData(bitmap, 0, 0, 0, 0, canvas.width, canvas.height);
+        renderGreenBlueGradient(greenOffset, blueOffset);
+        blueOffset++;
+        greenOffset += 2;
+        displayBuffer();
         requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
 }
 
 function load() {
-    document.body.appendChild(canvas);
-    window.onresize = resize;
+    document.body.appendChild(screen);
+    window.onresize = resizeWindow;
     window.onfocus = focus;
-    resize();
+    resizeBuffer(480, 270);
+    resizeWindow();
     main();
 }
 

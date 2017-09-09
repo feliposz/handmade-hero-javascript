4dc3df6be13acaa68243ae27c0db40507b6d844f
feliposz <feliposz@gmail.com>
2017-09-09 11:58:15 -0300

    006 - Gamepad and Keyboard input

--- code/main_handmade.js
+++ code/main_handmade.js
@@ -9,10 +9,10 @@ var BPP = 4; // Bytes per pixel
 function renderGreenBlueGradient(greenOffset, blueOffset) {
     var x, y, r, g, b, a, rowOffset, columnOffset;
     for (y = 0; y < bitmap.height; y++) {
-        rowOffset = y*bitmap.width*BPP;
+        rowOffset = y * bitmap.width * BPP;
         for (x = 0; x < bitmap.width; x++) {
-            columnOffset = rowOffset + x*BPP;
-            r = Math.floor(y*255/bitmap.height);
+            columnOffset = rowOffset + x * BPP;
+            r = Math.floor(y * 255 / bitmap.height);
             g = (x + greenOffset) % 256;
             b = (y + blueOffset) % 256;
             a = 255;
@@ -26,7 +26,9 @@ function renderGreenBlueGradient(greenOffset, blueOffset) {
 
 function displayBuffer() {
     buffer.getContext("2d").putImageData(bitmap, 0, 0, 0, 0, buffer.width, buffer.height);
-    screen.getContext("2d").drawImage(buffer, 0, 0);
+    var ctx = screen.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(buffer, 0, 0);
 }
 
 function resizeBuffer(width, height) {
@@ -37,8 +39,8 @@ function resizeBuffer(width, height) {
 
 function resizeWindow() {
     screen.width = window.innerWidth;
-    screen.height = window.innerHeight;    
-    screen.getContext("2d").scale(screen.width/buffer.width, screen.height/buffer.height);
+    screen.height = window.innerHeight;
+    screen.getContext("2d").scale(screen.width / buffer.width, screen.height / buffer.height);
     console.log("resize");
 }
 
@@ -46,25 +48,137 @@ function focus() {
     console.log("focus");
 }
 
+var DEADZONE = 0.25;
+
+function handleGamepad(controller) {
+    var i, pad, pads = navigator.getGamepads();
+    for (i = 0; i < pads.length; i++) {
+        pad = pads[i];
+        if (pad && pad.connected) {
+            controller.lStickX = Math.abs(pad.axes[0]) > DEADZONE ? pad.axes[0] : 0;
+            controller.lStickY = Math.abs(pad.axes[1]) > DEADZONE ? pad.axes[1] : 0;
+            controller.rStickX = Math.abs(pad.axes[2]) > DEADZONE ? pad.axes[2] : 0;
+            controller.rStickY = Math.abs(pad.axes[3]) > DEADZONE ? pad.axes[3] : 0;
+            controller.lTriggerValue = pad.buttons[6].value;
+            controller.rTriggerValue = pad.buttons[7].value;
+            controller.lStickThumb = pad.buttons[10].pressed;
+            controller.rStickThumb = pad.buttons[11].pressed;
+            controller.up = pad.buttons[12].pressed;
+            controller.down = pad.buttons[13].pressed;
+            controller.left = pad.buttons[14].pressed;
+            controller.right = pad.buttons[15].pressed;
+            controller.start = pad.buttons[8].pressed;
+            controller.back = pad.buttons[9].pressed;
+            controller.lShoulder = pad.buttons[4].pressed;
+            controller.rShoulder = pad.buttons[5].pressed;
+            controller.lTriggerButton = pad.buttons[6].pressed;
+            controller.rTriggerButton = pad.buttons[7].pressed;
+            controller.aButton = pad.buttons[0].pressed;
+            controller.bButton = pad.buttons[1].pressed;
+            controller.xButton = pad.buttons[2].pressed;
+            controller.yButton = pad.buttons[3].pressed;
+        }
+    }
+}
+
+function handleKeyboard(controller, pressed, keyCode) {
+    //console.log(pressed, keyCode);
+    switch (keyCode) {
+        case 65: controller.lStickX = pressed ? -1 : 0; break; // A
+        case 68: controller.lStickX = pressed ? +1 : 0; break; // D
+        case 83: controller.lStickY = pressed ? -1 : 0; break; // S
+        case 87: controller.lStickY = pressed ? +1 : 0; break; // W
+        case 74: controller.rStickX = pressed ? -1 : 0; break; // I
+        case 76: controller.rStickY = pressed ? -1 : 0; break; // J
+        case 75: controller.rStickX = pressed ? +1 : 0; break; // K
+        case 73: controller.rStickY = pressed ? +1 : 0; break; // L
+        case 188: controller.lTriggerValue = pressed ? +1 : 0; break; // .
+        case 190: controller.rTriggerValue = pressed ? +1 : 0; break; // ,
+        case 82: controller.lStickThumb = pressed; break; // 
+        case 70: controller.rStickThumb = pressed; break; // F
+        case 38: controller.up = pressed; break; // up arrow
+        case 40: controller.down = pressed; break; // down arrow
+        case 37: controller.left = pressed; break; // left arrow
+        case 39: controller.right = pressed; break; // right arrow
+        case 32: controller.start = pressed; break; // space
+        case 27: controller.back = pressed; break; // escape
+        case 81: controller.lShoulder = pressed; break; // Q
+        case 69: controller.rShoulder = pressed; break; // E
+        case 90: controller.aButton = pressed; break; // Z
+        case 88: controller.bButton = pressed; break; // X
+        case 67: controller.xButton = pressed; break; // C
+        case 86: controller.yButton = pressed; break; // V
+    }
+}
+
+function Controller() {
+    this.lStickX = 0;
+    this.lStickY = 0;
+    this.rStickX = 0;
+    this.rStickY = 0;
+    this.lTriggerValue = 0;
+    this.rTriggerValue = 0;
+    this.lStickThumb = false;
+    this.rStickThumb = false;
+    this.up = false;
+    this.down = false;
+    this.left = false;
+    this.right = false;
+    this.start = false;
+    this.back = false;
+    this.lShoulder = false;
+    this.rShoulder = false;
+    this.aButton = false;
+    this.bButton = false;
+    this.xButton = false;
+    this.yButton = false;
+};
+
 function main() {
+
     var greenOffset = 0, blueOffset = 0;
+
+    var gameController = new Controller();
+    var keyController = new Controller();
+
+    resizeBuffer(480, 270);
+    resizeWindow();
+
+    document.body.appendChild(screen);
+    window.onresize = resizeWindow;
+    window.onfocus = focus;
+
+    window.onkeydown = function (evt) {
+        handleKeyboard(keyController, true, evt.keyCode);
+    };
+    window.onkeyup = function (evt) {
+        handleKeyboard(keyController, false, evt.keyCode);
+    };
+
     var loop = function () {
+        handleGamepad(gameController);
+
+        if (keyController.up) {
+            blueOffset += 5;
+        }
+        if (keyController.down) {
+            blueOffset -= 5;
+        }
+        if (keyController.left) {
+            greenOffset += 5;
+        }
+        if (keyController.right) {
+            greenOffset -= 5;
+        }
+
+        greenOffset -= gameController.lStickX * 5;
+        blueOffset -= gameController.lStickY * 5;
+
         renderGreenBlueGradient(greenOffset, blueOffset);
-        blueOffset++;
-        greenOffset += 2;
         displayBuffer();
         requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
 }
 
-function load() {
-    document.body.appendChild(screen);
-    window.onresize = resizeWindow;
-    window.onfocus = focus;
-    resizeBuffer(480, 270);
-    resizeWindow();
-    main();
-}
-
-window.onload = load;
+window.onload = main;

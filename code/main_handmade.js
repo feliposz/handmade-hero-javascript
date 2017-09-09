c7a48250010d0e7d5ed88f2d2754745e9eecc761
feliposz <feliposz@gmail.com>
2017-09-09 01:59:04 -0300

    004 - Animating buffer

--- code/main_handmade.js
+++ code/main_handmade.js
@@ -3,26 +3,29 @@
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 
+var bitmap;
 var BPP = 4; // Bytes per pixel
 
-function resize() {
-    var bitmap, x, y;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    bitmap = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    
+function renderGreenBlueGradient(greenOffset, blueOffset) {
+    var x, y, r, g, b, a;
     for (y = 0; y < bitmap.height; y++) {
         for (x = 0; x < bitmap.width; x++) {
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 0] = canvas.width % 256; // Red
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 1] = canvas.height % 256; // Green
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 2] = 128; // Blue
-            bitmap.data[y*bitmap.width*BPP + x*BPP + 3] = 255; // Alpha (transparency)
+            r = Math.floor(y * 255 / bitmap.height);
+            g = (x + greenOffset) % 256;
+            b = (y + blueOffset) % 256;
+            a = 255;
+            bitmap.data[y*bitmap.width*BPP + x*BPP + 0] = r;
+            bitmap.data[y*bitmap.width*BPP + x*BPP + 1] = g;
+            bitmap.data[y*bitmap.width*BPP + x*BPP + 2] = b;
+            bitmap.data[y*bitmap.width*BPP + x*BPP + 3] = a;
         }
     }
+}
 
-    ctx.putImageData(bitmap, 0, 0, 0, 0, canvas.width, canvas.height);
-
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    bitmap = ctx.getImageData(0, 0, canvas.width, canvas.height);
     console.log("resize");
 }
 
@@ -30,11 +33,22 @@ function focus() {
     console.log("focus");
 }
 
+function main() {
+    var greenOffset = 0, blueOffset = 0;
+    var loop = function () {
+        renderGreenBlueGradient(greenOffset++, blueOffset++);
+        ctx.putImageData(bitmap, 0, 0, 0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+}
+
 function load() {
     document.body.appendChild(canvas);
     window.onresize = resize;
     window.onfocus = focus;
     resize();
+    main();
 }
 
 window.onload = load;
